3c25d6c4585ea0e9bae329c4bb2355501db0ef29
Dinithi Sonali Gamage <75924676+Dini-s@users.noreply.github.com>
2026-02-01 22:47:31 +0530

    project setup using vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <div>
+        <h1>Hello This is Dini port folio </h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
